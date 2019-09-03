2be31a1eaa19f0df0c3076ddd8cd7d80df379957
menkan <xtz17515@163.com>
2019-09-03 18:23:09 +0800

    deepCopy50%

--- utils/common.js
+++ utils/common.js
@@ -2,12 +2,58 @@
  * Craeted by 2019/06/18 10:40;
  * Descriptions: common utilTool
 */
-const common = {
-  auther: 'menKan'
+
+function forEach(array, callback) {
+  let index = -1
+  const lengths = array.length
+  while(++index < lengths) {
+    callback(array[index], index)
+  }
+  return array
 }
 
-common.sum = function sum() {
-  return '1111'
+class common {
+  constructor() {
+    this.auther = 'menkan_mark'
+    this.createdTime = '2019/09/03'
+  }
+
+  fillZero(number) {
+    return '' + (number >= 10 ? number : `0${number}`)
+  }
+
+  getTypeof(param) {
+    let toString = Object.prototype.toString
+    let str = toString.call(param)
+    return str.substring(8, str.length - 1)
+  }
+
+  // 深拷贝
+  deepCopy(param, map = new WeakMap()) {
+    if(typeof param === 'object') {
+      const isArray = Array.isArray(param)
+      let temporary = isArray ? [] : {}
+      if (map.get(param)) {
+        return map.get(param)
+      }
+      map.set(param, temporary)
+      const keys = isArray ? undefined : Object.assign(param)
+      // for(const key in param) {
+      //   temporary[key] = this.deepCopy(param[key], map)
+      // }
+      forEach(keys || param, (value, key) => {
+        console.log('keys>>>', keys)
+        if (keys) {
+          key = value;
+        }
+        temporary[key] = this.deepCopy(param[key], map)
+      })
+      return temporary
+    } else {
+      return param
+    }
+  }
+
 }
 
-module.exports = common
+module.exports = new common
--- views/demos/index.js
+++ views/demos/index.js
@@ -1,4 +1,35 @@
 const path = require('path')
 const commonTool = require(path.join(__dirname, '../../utils/common.js'))
 
-console.log(commonTool)
+// console.log(commonTool.fillZero(19))
+
+// console.log(commonTool.getTypeof(123))
+
+// 深拷贝
+let originObj = {
+  field1: 1,
+  field2: undefined,
+  field3: 'ConardLi',
+  field4: {
+      child: 'child',
+      child2: {
+          child2: 'child2'
+      }
+  },
+  field5: [1, 2, 3]
+}
+originObj.originObj = originObj
+
+console.time()
+let copyObj = commonTool.deepCopy(originObj)
+console.log('copy print >>>>   ', copyObj)
+console.timeEnd()
+
+
+console.log(Object.keys({
+  a: '1',
+  b: '2',
+  c: 3,
+  d: 4,
+  e: 5
+}))
